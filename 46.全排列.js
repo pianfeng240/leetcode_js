e12298baa779a85aac33ccd3892f4a52603a68b8
xzj <252228148@qq.com>
2020-07-29 08:26:23 +0800

    回溯算法解决全排列

---
+++ 46.全排列.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=javascript
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    const res = []
+
+    const backtrack = (path, nums) => {
+        if (path.length === nums.length) {
+            res.push(path.slice())
+            return
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            const element = nums[i];
+            if (path.includes(element)) {
+                continue;
+            }
+            path.push(element)
+            backtrack(path, nums)
+            path.pop(element)
+        }
+    }
+
+    backtrack([], nums)
+
+    return res;
+};
+// @lc code=end
+
